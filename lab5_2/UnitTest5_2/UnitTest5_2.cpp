444dd0cd5f531cf18f9acb1b190301fd8052308c
KlikSen <ppsha27012003@gmail.com>
2020-11-15 13:11:15 +0300

    5.2

---
+++ lab5_2/UnitTest5_2/UnitTest5_2.cpp
@@ -0,0 +1,20 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab5_2.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest52
+{
+	TEST_CLASS(UnitTest52)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int t;
+			t = A(2, 3, 1);
+			Assert::AreEqual(t, -1);
+		}
+	};
+}

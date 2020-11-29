5cc95974328d1f0de7905a396f05ccf05b746758
KlikSen <ppsha27012003@gmail.com>
2020-11-29 15:38:26 +0300

    1

--- lab6_3(2)/lab6_3(2).cpp
+++ lab6_3(2)/lab6_3(2).cpp
@@ -44,7 +44,7 @@ void Print(int* mas, const int size, int i)
         cout << endl;
 }
 template <typename T>
-void Max2(T* mas, const int size, int& max, int& imax, int j)
+void Max2(T* mas, const int size, T& max, int& imax, int j)
 {
     if (max < mas[j])
     {
@@ -67,4 +67,47 @@ void Sort2(T* mas, const int size, int i)
 
     if (i < size - 2)
         Sort2(mas, size, i + 1);
+}
+
+template <typename T>
+void Print2(T* mas, const int size, int i)
+{
+    cout << setw(4) << mas[i];
+    if (i < size - 1)
+        Print(mas, size, i + 1);
+    else
+        cout << endl;
+}
+template <typename T>
+void Create2(T* mas, const int size, const T Low, const T High, int i)
+{
+    mas[i] = Low + rand() % (High - Low + 1);
+    if (i < size - 1)
+        Create(mas, size, Low, High, i + 1);
+}
+
+int main()
+{
+    SetConsoleOutputCP(1251);
+    srand((unsigned)time(NULL));
+    int c = -50;
+    int b = 50;
+
+    const int N = 30;
+    int* a = new int[N];
+    int* t = new int[N];
+
+    Create(a, N, c, b, 0);
+    Print(a, N, 0);
+    Sort(a, N, 0);
+    Print(a, N, 0);
+
+    cout << endl;
+
+    Create2(a, N, c, b, 0);
+    Print2(a, N, 0);
+    Sort2(a, N, 0);
+    Print2(a, N, 0);
+
+    return 0;
 }
